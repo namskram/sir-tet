c4a5493fe5a1abd258e56af175b3a75cc2c2792e
namskram <wildbiduuf@gmail.com>
2024-11-27 15:44:33 -0800

    added code for L1

---
+++ sir-tet/src/mino/Mino_L2.java
@@ -0,0 +1,15 @@
+package mino;
+
+import java.awt.Color;
+
+public class Mino_L2 extends Mino {
+
+    public Mino_L2() {
+        create(Color.orange);
+    }
+
+    public void setXY(int x, int y) {
+        
+    }
+
+}
---
+++ sir-tet/src/mino/Mino_T.java
@@ -0,0 +1,14 @@
+package mino;
+
+import java.awt.Color;
+
+public class Mino_T extends Mino {
+
+    public Mino_T() {
+        create(Color.orange);
+    }
+
+    public void setXY(int x, int y) {
+        
+    }
+}
